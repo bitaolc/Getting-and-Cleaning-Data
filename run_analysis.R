25e68496db98a4d87bb78465b819d21144c8e252
bitaolc <manjusaka89@gmail.com>
2018-03-19 00:46:20 -0400

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,61 @@
+#Getting and Cleaning Data Week 4 Assignment
+
+#Download Data
+setwd("E:/Folder/Coursera/R/Getting and Cleaning Data/Week4/assignment")
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl, destfile=paste0(getwd(),"/Dataset.zip"))
+
+# Unzip dataSet to /data directory
+unzip(zipfile="/Dataset.zip")
+
+# Reading trainings tables:
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+
+# Reading testing tables:
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+
+# Reading feature vector:
+features <- read.table("UCI HAR Dataset/features.txt")
+
+# Reading activity labels:
+activityLabels = read.table("UCI HAR Dataset/activity_labels.txt")
+
+#add column name
+colnames(x_train) <- features[,2] 
+colnames(y_train) <-"activityId"
+colnames(subject_train) <- "subjectId"
+
+colnames(x_test) <- features[,2] 
+colnames(y_test) <- "activityId"
+colnames(subject_test) <- "subjectId"
+
+colnames(activityLabels) <- c('activityId','activityType')
+
+#merge datasets
+mrg_train <- cbind(y_train, subject_train, x_train)
+mrg_test <- cbind(y_test, subject_test, x_test)
+setAllInOne <- rbind(mrg_train, mrg_test)
+colNames <- colnames(setAllInOne)
+str(setAllInOne)
+
+#mean and standard deviation
+mean_and_std <- (grepl("activityId" , colNames) | 
+                   grepl("subjectId" , colNames) | 
+                   grepl("mean.." , colNames) | 
+                   grepl("std.." , colNames) 
+)
+MeanAndStd <- setAllInOne[ , mean_and_std == TRUE]
+
+#activity
+ActivityNames <- merge(MeanAndStd, activityLabels,
+                       by='activityId',
+                       all.x=TRUE)
+
+#create tidy dataset
+TidySet <- aggregate(. ~subjectId + activityId, ActivityNames, mean)
+TidySet <- TidySet[order(TidySet$subjectId, TidySet$activityId),]
+write.table(TidySet, "TidySet.txt", row.name=FALSE)
